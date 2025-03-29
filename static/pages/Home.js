b48aadd0b4f953401ffa7d967b4ab52e66945d6d
rubix <akshobh.verma@gmail.com>
2025-03-29 02:24:40 +0000

    Improved Home Page

--- static/pages/Home.js
+++ static/pages/Home.js
@@ -1,5 +1,20 @@
 const Home = {
-    template: `<h1>This is the home page</h1>`
+    template: `
+        <div class="d-flex flex-column justify-content-center align-items-center vh-100 bg-light">
+
+            <div class="text-center" style="transform: translateY(-25%);">
+                <h1 class="text-black px-4 py-3 fw-bold" style="font-size: 6rem;">Welcome to Quizzy</h1>
+                <button @click="redirectLogin" class="btn btn-primary mt-3 px-4 py-2 fs-4">Go to Login Page</button>
+            </div>
+
+        </div>
+    `,
+
+    methods: {
+        redirectLogin() {
+            this.$router.push('/login')
+        }
+    }
 }
 
 export default Home;
